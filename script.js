195468d1f6070f1adb87cd9aae25b9030ad9e3f9
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2024-01-31 14:20:32 -0500

    Scroll through text

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
 
 var imageIndex = 0;
+
+var dogItemsObj = { 
+    items: [
+        {
+            image: "./dog_shampoo.jpeg", 
+            title: "<strong> BK Dog Shampoo </strong>", 
+            description: "Organic, made from only natural ingredients. <br/> Affordable, while being good for you dog's skin",
+            description2: "<li> Frangrence Free</li> <li> Contains Aloe </li> <li> Helps with shedding </li>"
+        } ,
+        {
+            image: "./dog_collar.jpg", 
+            title: "<strong> BK Dog Collar </strong>", 
+            description: "Quality material collar that does <br/> not choke the dog",
+            description2: "<li> Fragrence Free </li>  <li> Buckle support</li> <li> Optional name tag </li>"
+        } ,
+        {
+            image: "./dog_leash.jpeg", 
+            title: "<strong> BK Dog Leash </strong>", 
+            description: "Sturdy leash for taking your <br/> dog on walks",
+            description2: "<li> Fragrence free </li> <li>various lengths</li> <li>various colors </li>"
+        } ,
+    ]
+
+}
+
 function main() {
 
     ///Changes an image in the image caurosel
     ///Parameter: isReverse[Boolean]
     function changeImage(isReverse) {
-      let imgAry = ["./dog_shampoo.jpeg","./dog_collar.jpg", "./dog_leash.jpeg"];
       var dogShampooImg = document.getElementById("dog_shampoo");
       imageIndex++;
-      imageIndex = imageIndex % imgAry.length
-      dogShampooImg.src = imgAry[imageIndex];
+      imageIndex = imageIndex % dogItemsObj.items.length;
+      var dogObj =  dogItemsObj.items[imageIndex];
+      dogShampooImg.src = dogObj.image;
     }
     function changeText(isReverse) {
+        var title = document.getElementById("title");
+        var line1 = document.getElementById("line1");
+        var list_items = document.getElementById("list");
+    
 
+        var dogObj =  dogItemsObj.items[imageIndex];
+        title.innerHTML = dogObj.title;
+        line1.innerHTML = dogObj.description;
+        list_items.innerHTML = dogObj.description2;
+        
     }
     var leftArrow = document.getElementById("leftArrow");
     var rightArrow = document.getElementById("rightArrow");
 
     leftArrow.addEventListener("click", function(event){
         changeImage()
+        changeText()
     });
     rightArrow.addEventListener("click", function(event) {
         changeImage()
+        changeText()
     });
 
 }
